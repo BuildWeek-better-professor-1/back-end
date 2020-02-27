667cff6bf89ce4d51a8a72541a530610f38f3694
Buddy Agyin <agyin3@gmail.com>
2020-02-26 21:12:34 -0700

    create middleware to validate info in database

---
+++ custom-middleware/validateStudentById.js
@@ -0,0 +1,20 @@
+const Students = require('../students/student-model.js')
+
+module.exports = async function(req, res, next){
+    const { id } = req.params
+    console.log(id)
+    try {
+        const student = await Students.findStudentById(id)
+        if(student){
+            req.student = student
+            next()
+        }else {
+            res.status(400).json({message: 'Invalid Student ID'})
+        }
+    }catch(err){
+        res.status(500).json({
+            error: err,
+            errorMessage: `There was an error with your ${req.method} request`
+        })
+    }
+}
