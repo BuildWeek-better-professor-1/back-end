1415b30d2d15146b07bbc0b92eaecda077b791c9
Buddy Agyin <agyin3@gmail.com>
2020-02-27 13:17:13 -0700

    add route to remove student

--- routers/student-router.js
+++ routers/student-router.js
@@ -8,9 +8,38 @@ router.use('/:id', validateStudentById)
 router.get('/:id', (req,res) => {
     res.status(200).json({
         data: {
-            student: req.student
+            student: {
+                "id": req.student.id,
+                "First Name": req.student.firstName,
+                "Last Name": req.student.lastName
+            }
         }
     })
+})
+
+router.delete('/:id', (req, res) => {
+    const {id} = req.params
+
+    Students.removeStudent(id)
+        .then(() => {
+            res.status(200).json({
+                data: {
+                    message: 'Student Successfully deleted',
+                    student: {
+                        "id": req.student.id,
+                        "First Name": req.student.firstName,
+                        "Last Name": req.student.lastName
+                    }
+                }
+
+            })
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err,
+                errorMessage: `There was an error with your ${req.method} requestt`
+            })
+        })
 })
 
 module.exports = router
