086360b5ed739eb84da16058ee420a3915c0bdde
Buddy Agyin <agyin3@gmail.com>
2020-02-27 20:48:25 -0700

    add student users seeds

---
+++ database/seeds/02-studentUsers.js.js
@@ -0,0 +1,12 @@
+
+exports.seed = function(knex, Promise) {
+  return knex('studentUsers').truncate()
+    .then(function () {
+      // Inserts seed entries
+      return knex('studentUsers').insert([
+        {id: 1, firstName: 'Kobe', email: 'kobe@lakers.com', username: 'kb24', password: 'mambamentality', lastName: 'Bryant', type: 'student', profId: 3},
+        {id: 2, firstName: 'Lebron', email: 'lebron@lakers.com', username: 'kingjames', password: 'kingjames', lastName: 'James', type: 'student', profId: 2},
+        {id: 3, firstName: 'Anthony', email: 'anthony@lakers.com', username: 'thebrow', password: 'thebrow', lastName: 'Davis', type: 'student', profId: 1},
+      ]);
+    });
+};
