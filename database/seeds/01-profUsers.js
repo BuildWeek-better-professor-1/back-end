870a0bd9f9471dc0cab28d7085b0ac0037511424
Buddy Agyin <agyin3@gmail.com>
2020-02-27 20:47:17 -0700

    rename files

---
+++ database/seeds/01-profUsers.js
@@ -0,0 +1,12 @@
+
+exports.seed = function(knex, Promise) {
+  return knex('professorUsers').truncate()
+    .then(function () {
+      // Inserts seed entries
+      return knex('professorUsers').insert([
+        {id: 1, firstName: 'Harry', email: 'harry@hogwarts.com', username: 'harry', password: 'harrypotter', lastName: 'Potter', type: 'professor'},
+        {id: 2, firstName: 'Severus', email: 'severus@hogwarts.com', username: 'severus', password: 'severus', lastName: 'Snape', type: 'professor'},
+        {id: 3, firstName: 'Albus', email: 'albus@hogwarts.com', username: 'albus', password: 'kingalbus', lastName: 'Dumbledore', type: 'professor'},
+      ]);
+    });
+};
