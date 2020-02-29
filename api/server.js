61a74bb89601ec157476c1dc354bce6c03657fee
Buddy Agyin <agyin3@gmail.com>
2020-02-29 10:22:56 -0700

    add reminder router

--- api/server.js
+++ api/server.js
@@ -8,6 +8,7 @@ const profRouter = require('../routers/professor-router.js')
 const studentRouter = require('../routers/student-router.js')
 const projectRouter = require('../routers/project-router.js')
 const studentUsersRouter = require('../routers/studentUsers-router.js')
+const remindersRouter = require('../routers/reminders-router.js')
 
 const server = express()
 
@@ -19,6 +20,7 @@ server.use('/api/users/professor', profRouter)
 server.use('/api/users/students', studentUsersRouter)
 server.use('/api/students', restricted, studentRouter)
 server.use('/api/projects', restricted, projectRouter)
+server.use('/api/reminders', restricted, remindersRouter)
 
 server.get('/', (req, res) => {
     res.send({hello: `Let's get this f*cking party started`})
