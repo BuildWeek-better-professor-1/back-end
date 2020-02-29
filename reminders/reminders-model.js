1b3ce31ec580f9f11dcc154dda2865d0b0b9f455
Buddy Agyin <agyin3@gmail.com>
2020-02-29 00:52:29 -0700

    create reminder helper functions

---
+++ reminders/reminders-model.js
@@ -0,0 +1,52 @@
+const db = require('../database/dbConfig.js')
+
+module.exports = {
+    addReminder,
+    getReminders,
+    removeReminder,
+    updateReminder,
+    getRemindersById
+}
+
+function getReminders(){
+    return db('reminders as r')
+        .join('projects as p', 'p.id', 'r.projectId')
+        .select
+}
+
+function getRemindersById(id){
+    return db('reminders as r')
+        .join('projects as p', 'p.id', 'r.projectId')
+        .join('students as s', 's.id', 'p.studentId')
+        .select(
+            'r.id', 
+            'r.message', 
+            'r.date', 
+            'p.name as Project Name',
+            'p.dueDate as Due Date',
+            's.firstName as First Name',
+            's.lastName as Last Name',
+            's.id as Student Id'
+        )
+        .where('r.id', id)
+        .first()
+}
+
+function addReminder(info){
+    return db('reminders')
+        .insert(info, ['id'])
+        .then(id=> getRemindersById(id[0]))
+}
+
+function updateReminder(id, changes){
+    return db('reminders')
+        .update(changes)
+        .where({id})
+        .then(() => getRemindersById(id))
+}
+
+function removeReminder(id){
+    return db('reminders')
+        .del()
+        .where({id})
+}
